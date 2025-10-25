96de573c63c34967bbd208cedc4b1e8e95db9cf3
PrasannaKumar12-coder <prasannakumarmungalla665@gmail.com>
2025-10-25 10:33:32 +0530

    remove github things

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Aurum-Time/'  // <-- EXACT repo name (caps included)
+  plugins: [react()],  // <-- EXACT repo name (caps included)
 })
